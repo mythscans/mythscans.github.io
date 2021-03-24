fc38830035e04171eb8469bbba10cf40b2e68005
dualcus <78871554+mythscans@users.noreply.github.com>
2021-03-24 14:50:48 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,7 +8,7 @@ function imageExists(image_url){
     return http.status != 404;
 
   }
-  async function setChapterCookies(chapternumber,seriesname) {
+function setChapterCookies(chapternumber,seriesname) {
     var i = 1
     for(i = 1; imageExists(`./${chapternumber}/${i}.jpg`) && i < 150;i++) {}//increments i until no
     postMessage(`pages+${chapternumber}+${seriesname}=${i}; path=/`)
