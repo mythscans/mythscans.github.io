9e2603299ac2b068c7252ef22b5a53e9f946509b
dualcus <78871554+mythscans@users.noreply.github.com>
2021-03-24 14:26:11 -0500

    potentially fixed issue
    potentially fixed issue

--- worker.js
+++ worker.js
@@ -10,9 +10,8 @@ function imageExists(image_url){
   }
   async function setChapterCookies(chapternumber,seriesname) {
     var i = 1
-    if(document.cookie.includes(`pages+${chapternumber}+${seriesname}`)) return 0;
     for(i = 1; imageExists(`./${chapternumber}/${i}.jpg`) && i < 150;i++) {}//increments i until no
-    document.cookie = `pages+${chapternumber}+${seriesname}=${i}; path=/`
+    postMessage(`pages+${chapternumber}+${seriesname}=${i}; path=/`)
   }
   onmessage = function(e) {
     console.log('Message received from main script');
